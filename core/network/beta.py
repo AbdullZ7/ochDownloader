690240d08f6bcd3a4ea9dd076ae8b5116628485c
nitely <ochdownloader@gmail.com>
2012-05-14 20:43:36 -0300

    beta development continues

--- core/network/beta.py
+++ core/network/beta.py
@@ -22,6 +22,8 @@ DATA_BUFSIZ = 64 * 1024 #64K.
 
 
 class ChunkComplete(Exception): pass
+class BadSource(Exception): pass
+class CanNotRun(Exception): pass
 
 
 class MultiDownload(DownloaderCore):
@@ -55,52 +57,160 @@ class MultiDownload(DownloaderCore):
 
     # ////////////////////// Beta stuff...
 
-    def spawn_thread_(self, fh, i, chunks_tuple):
+    def spawn_thread(self, fh, i, chunks_tuple):
         """"""
         #Requested Range Not Satisfiable, bug.
         if chunks_tuple[1] is None and chunks_tuple[0] > 0:
             chunks_tuple = (chunks_tuple[0] - 1, None)
-        th = threading.Thread(group=None, target=self.thread_download_, name=None, args=(fh, i, chunks_tuple))
+        th = threading.Thread(group=None, target=self.thread_download, name=None, args=(fh, i, chunks_tuple))
         th.start()
         return th
 
-    def threaded_download_manager_(self, fh):
+    def create_chunks(self):
+        chunk_size = (self.size_file / MAX_CONN) + (self.size_file % MAX_CONN)
+        chunk_size = ((chunk_size / BUFFER_SIZE) + 1) * BUFFER_SIZE #proximo numero al tamanio del chunk que sea multiplo del buffer
+        chunks = []
+        start = 0
+        while True:
+            end = start + chunk_size if (start + chunk_size) < self.size_file else None #start + chunk_size - 1 to avoid overlapping
+            chunks.append((start, end))
+            start += chunk_size
+            if end is None:
+                break
+        return chunks
+
+    def __get_chunks_size_complete(self):
+        complete = 0
+        tmp = 0
+        for chunk_tup in self.chunks:
+            complete += chunk_tup[0] - tmp
+            tmp = chunk_tup[1]
+        return complete
+
+    def threaded_download_manager(self, fh):
         """"""
         if not self.chunks or not self.file_exists:
-            chunk_size = (self.size_file / MAX_CONN) + (self.size_file % MAX_CONN)
-            chunk_size = ((chunk_size / BUFFER_SIZE) + 1) * BUFFER_SIZE #proximo numero al tamanio del chunk que sea multiplo del buffer
-            chunks_ = []
-            start = 0
-            while True:
-                end = start + chunk_size if (start + chunk_size) < self.size_file else None #start + chunk_size - 1 to avoid overlapping
-                chunks_.append((start, end))
-                if end is not None:
-                    start += chunk_size
-                else:
-                    break
-            self.chunks = chunks_
+            self.chunks = self.create_chunks()
         else: #resume
-            complete = 0
-            tmp = 0
-            for chunk_tuple in self.chunks:
-                complete += chunk_tuple[0] - tmp
-                tmp = chunk_tuple[1]
-            self.size_complete = complete
+            self.size_complete = self.__get_chunks_size_complete()
 
         self.chunks_control = [True for _ in self.chunks] #can_run
 
-        th_list = [self.spawn_thread_(fh, i, chunks_tuple) for i, chunks_tuple in enumerate(self.chunks[:])
+        th_list = [self.spawn_thread(fh, i, chunks_tuple) for i, chunks_tuple in enumerate(self.chunks[:])
                    if chunks_tuple[1] is None or chunks_tuple[0] < chunks_tuple[1]] #range_start may be bigger than range_end at the end by 1 byte (1025, 1024) coz it starts on 0.
 
         for th in th_list:
             th.join()
 
-    def thread_download_(self, fh, i, chunk_range):
+    def is_master(self, chunk): #or pass is_master to thread_download
+        master = True if chunk[0] == self.content_range else False
+        return master
+
+    def is_valid(self, source):
+        info = source.info()
+        if self.size_file == self.get_content_size(info):
+            return True
+        return False
+
+
+    def get_source(self, chunk):
+        #if self.is_master():
+            #source = self.source
+        source = self.source
+        return source
+
+    def thread_download_(self, fh, i, chunk):
+        #master thread wont retry.
+        buffer_list = []
+        len_buffer_data = 0
+        complete = 0
+        def flush_buffer():
+            buf_data = ''.join(buffer_list)
+            try:
+                with self.lock1:
+                    fh.seek(chunk[0] + complete - len_buffer_data)
+                    fh.write(buf_data)
+                with self.lock2:
+                    start = chunk[0] + complete
+                    self.chunks[i] = (start, self.chunks[i][1])
+                del buffer_list[:]
+                len_buffer_data = 0
+            except EnvironmentError as err:
+                logger.exception(err)
+                self.error_flag = True
+                self.status_msg = "Error: {0}".format(err)
+
+        for retry in range(3):
+            try:
+
+                with URLClose(self.get_source(chunk)) as s:
+                    if not self.is_master(chunk) and not self.is_valid(s):
+                        raise BadSource('Link expired, or cant download the requested range.')
+
+                    with self.lock3:
+                        if self.chunks_control[i]:
+                            self.chunks_control[i] = False
+                        else:
+                            raise CanNotRun('Another thread has taken over this chunk.')
+
+                    while True:
+                        data = s.read(NT_BUFSIZ)
+                        len_data = len(data)
+
+                        buffer_list.append(data)
+                        len_buffer_data += len_data
+                        complete += len_data
+
+                        if len_buffer_data >= DATA_BUFSIZ:
+                            flush_buffer()
+
+                        with self.lock2:
+                            self.size_complete += len_data
+
+                        if complete >= chunk[1] - chunk[0]:
+                            i += 1
+                            with self.lock2: #safe.
+                                with self.lock3:
+                                    try:
+                                        if self.chunks_control[i] and chunk[1] == self.chunks[i][0]: #on resume, end from the current segment must be equal to start from the next one.
+                                            chunk = self.chunks[i]
+                                            self.chunks_control[i] = False
+                                        #elif self.chunks_control[i]:
+                                            #raise CantResume('cant resume next chunk')
+                                        else:
+                                            return
+                                    except IndexError:
+                                        return
+                            complete = 0
+                            len_buffer_data = 0
+
+
+
+
+            except (BadSource, CanNotRun) as err:
+                #not master
+                #propagate
+                return
+            except (urllib2.URLError, httplib.HTTPException, socket.error) as err:
+                #if self.is_master():
+                    #propagate
+                #else:
+                    #retry
+                return
+            except EnvironmentError as err:
+                #propagate
+                return
+            finally:
+                flush_buffer()
+
+
+    def thread_download(self, fh, i, chunk_range):
         print "beta"
         complete = 0
         running_flag = False
         while True:
             try:
+                #if not is_resuming: self.content_range = 0
                 if chunk_range[0] == self.content_range and not running_flag:
                     s = self.source
                     running_flag = True
@@ -167,12 +277,7 @@ class MultiDownload(DownloaderCore):
                             chunk_range = self.chunks[i]
                         complete = 0
 
-            except (urllib2.URLError, httplib.HTTPException, socket.error) as err:
-                logger.warning("Conexion {0}: {1}".format(i, err))
-                time.sleep(3) #to avoid overflowing the server.
-                if self.stop_flag or self.error_flag:
-                    return
-            except EnvironmentError as err: #excepciones en Threads no pueden ser delegadas.
+            except Exception as err: #excepciones en Threads no pueden ser delegadas.
                 logger.exception(err)
                 self.error_flag = True
                 self.status_msg = "Error: {0}".format(err) #operaciones atomicas. No necesita lock.
