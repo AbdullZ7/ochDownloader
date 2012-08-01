9dd07b16252021330c75ebeb2e7df402db622ba5
nitely <ochdownloader@gmail.com>
2012-08-01 20:46:41 -0300

    clean up

--- plugins/mediafire/anonym_download.py
+++ plugins/mediafire/anonym_download.py
@@ -24,17 +24,3 @@ class PluginDownload(PluginsCore):
 
 if __name__ == "__main__":
     pass
-    import re
-    page = '"http://some" kNO = "http://205.196.123.44/27girlme3cxg/asdasdasdasd/some.rar";'
-    pattern = '([^"]+/asdasdasdasd/[^"]+)'
-    m = re.search(pattern, page, re.S)
-    if m is not None:
-        print m.groups()
-    else:
-        print 'not found'
-    
-
-
-
-
-
