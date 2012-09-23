c266bdb2472861213f5abecf27a726c4c9a9ac6c
nitely <ochdownloader@gmail.com>
2012-09-23 17:03:27 -0300

    get file name

--- plugins/sourceforge/link_checker.py
+++ plugins/sourceforge/link_checker.py
@@ -8,8 +8,8 @@ class LinkChecker:
     """"""
     def check(self, link):
         """"""
-        name = "Unknown"
         size = 0
         status_msg = None
-        link_status = cons.LINK_ERROR
+        link_status = cons.LINK_ALIVE
+        name = link.split("/")[-2]
         return link_status, name, size, status_msg
