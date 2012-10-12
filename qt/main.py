adbe0dd070b081b95be9d19744981d673d6c07dd
nitely <ochdownloader@gmail.com>
2012-10-12 19:00:21 -0300

    set downloads on close preferences roll back

--- qt/main.py
+++ qt/main.py
@@ -239,7 +239,7 @@ class Gui(QMainWindow):
         index_page = self.tab.indexOf(self.preferences)
         if index_page >= 0:
             self.tab.removeTab(index_page)
-        self.tab.setCurrentIndex(0)
+        #self.tab.setCurrentIndex(0)
     
     def on_about(self):
         about = About(self)
