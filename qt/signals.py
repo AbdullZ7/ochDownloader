d024ae77a1ce164edb9bb0f9274de1a209a4b535
nitely <ochdownloader@gmail.com>
2012-10-20 23:47:53 -0300

    weak ref implementation

--- qt/signals.py
+++ qt/signals.py
@@ -1,18 +1,40 @@
 import threading
+import weakref
 import logging
 logger = logging.getLogger(__name__)
 
 from core import idle_queue
 
 
+class BoundMethodWeakref:
+    def __init__(self, func):
+        self.func_name = func.__name__
+        self.wref = weakref.ref(func.__self__) #http://docs.python.org/reference/datamodel.html
+
+    def __call__(self):
+        func_obj = self.wref()
+        if func_obj is None: #lost reference
+            return None
+        else:
+            func = getattr(func_obj, self.func_name)
+            return func
+
+def weak_ref(callback):
+    if hasattr(callback, '__self__') and callback.__self__ is not None: #is a bound method?
+        return BoundMethodWeakref(callback)
+    else:
+        return weakref.ref(callback)
+
+
 class Event:
-    def __init__(self, name=None):
+    def __init__(self, name):
         self.name = name
         self.callbacks = []
         self.lock = threading.Lock()
 
     def connect(self, callback):
         with self.lock:
+            callback = weak_ref(callback)
             self.callbacks.append(callback)
 
     def disconnect(self, callback):
@@ -27,8 +49,10 @@ class Event:
                 #connected_methods = [callback.__name__ for callback in self.callbacks]
                 logger.debug("Event emitted: {}".format(self.name))
 
-            for callback in self.callbacks:
-                idle_queue.idle_add(callback, *args, **kwargs)
+            for weakref_callback in self.callbacks:
+                callback = weakref_callback()
+                if callback is not None:
+                    idle_queue.idle_add(callback, *args, **kwargs)
 
 
 class Signals:
