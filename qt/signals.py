ecbbc8af603f4a9634b62baafc969fbd704ce15f
nitely <ochdownloader@gmail.com>
2012-10-22 12:20:35 -0300

    weak ref improvements

--- qt/signals.py
+++ qt/signals.py
@@ -6,22 +6,26 @@ logger = logging.getLogger(__name__)
 from core import idle_queue
 
 
-class BoundMethodWeakref:
+class _BoundMethodWeakref:
     def __init__(self, func):
         self.func_name = func.__name__
-        self.wref = weakref.ref(func.__self__) #http://docs.python.org/reference/datamodel.html
+        self.wref = weakref.ref(func.__self__) #__self__ returns the class http://docs.python.org/reference/datamodel.html
 
     def __call__(self):
-        func_obj = self.wref()
-        if func_obj is None: #lost reference
+        func_cls = self.wref()
+        if func_cls is None: #lost reference
             return None
         else:
-            func = getattr(func_obj, self.func_name)
+            func = getattr(func_cls, self.func_name)
             return func
+
+    def __cmp__(self, other):
+        func = self.__call__()
+        return cmp(func, other)
 
 def weak_ref(callback):
     if hasattr(callback, '__self__') and callback.__self__ is not None: #is a bound method?
-        return BoundMethodWeakref(callback)
+        return _BoundMethodWeakref(callback)
     else:
         return weakref.ref(callback)
 
