5c5fa3fdbc91c789e475129534a8079ee8827f8b
nitely <ochdownloader@gmail.com>
2012-11-12 14:51:48 -0300

    flashgot parse commands 2

--- addons/flashgot/parser.py
+++ addons/flashgot/parser.py
@@ -2,12 +2,11 @@ import argparse
 
 
 class ParseArgs:
-    def __init__(self, data):
-        #untested
-        parser = argparse.ArgumentParser.parse_args(data)
-        parser.add_argument('--links', dest='links', required=True)
-        args = parser.parse_args()
-        print args.links
+    def __init__(self, args):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-l', '--links', dest='links', required=True)
+        p = parser.parse_args(args=args)
+        print p.links
 
     def send_downloads(self):
         #TODO emit signal, passing all links
