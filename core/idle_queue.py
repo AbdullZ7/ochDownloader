5c87537c970c350594a5090dc1cb448189c85f95
nitely <ochdownloader@gmail.com>
2012-11-20 22:10:11 -0300

    clean up

--- core/idle_queue.py
+++ core/idle_queue.py
@@ -1,8 +1,7 @@
 import threading
 import Queue
 import logging
-logger = logging.getLogger(__name__) #__name___ = nombre del modulo. logging.getLogger = Usa la misma instancia de clase (del starter.py).
-
+logger = logging.getLogger(__name__)
 
 #thread safety
 _thread_lock = threading.Lock()
@@ -10,11 +9,13 @@ _event_list = []
 _block = False
 idle_loop = Queue.Queue()
 
+
 def idle_add(func, *args, **kwargs):
     def idle():
         func(*args, **kwargs)
         return False
     idle_loop.put(idle)
+
 
 def idle_add_and_wait(func, *args, **kwargs):
     event = threading.Event()
@@ -30,12 +31,14 @@ def idle_add_and_wait(func, *args, **kwargs):
         event.wait() #wait for set().
     remove_event(event)
 
+
 def set_events(): #execute at exit.
     logger.debug("Setting pending events.")
     _block = True
     with _thread_lock:
         for event in _event_list:
             event.set()
+
 
 def register_event(event):
     with _thread_lock:
@@ -44,12 +47,14 @@ def register_event(event):
         return True
     return False
 
+
 def remove_event(event):
     try:
         with _thread_lock:
             _event_list.remove(event)
     except ValueError:
         pass
+
 
 
 if __name__ == "__main__":
@@ -61,4 +66,3 @@ if __name__ == "__main__":
         callback()
     except Queue.Empty as err:
         print err
-
