cc2c25b5ac25897a155fcb0e51485d8e543862b6
nitely <ochdownloader@gmail.com>
2012-12-05 14:20:22 -0300

    save_as on session

--- core/api.py
+++ core/api.py
@@ -67,7 +67,7 @@ class _Api(DownloadManager, AddDownloadsManager):
             ordered_list = []
             download_list = self.session_parser.load()
             for item in download_list:
-                FILE_NAME, FILE_PATH, LINK, HOST, SIZE, PROGRESS, TIME, CHUNCKS, QUALITY = range(9)
+                FILE_NAME, FILE_PATH, LINK, HOST, SIZE, PROGRESS, TIME, CHUNCKS, QUALITY, SAVE_AS = range(10)
                 download_item = DownloadItem(item[FILE_NAME], item[HOST], item[SIZE], item[LINK], item[FILE_PATH])
                 download_item.status = cons.STATUS_STOPPED
                 download_item.progress = item[PROGRESS]
@@ -75,6 +75,7 @@ class _Api(DownloadManager, AddDownloadsManager):
                 download_item.time = item[TIME]
                 download_item.chunks = item[CHUNCKS]
                 download_item.video_quality = item[QUALITY]
+                download_item.save_as = item[SAVE_AS]
                 self.stopped_downloads[download_item.id] = download_item
                 ordered_list.append(download_item)
         except Exception as err:
@@ -89,7 +90,7 @@ class _Api(DownloadManager, AddDownloadsManager):
             if download_item is not None:
                 download_list.append([download_item.name, download_item.path, download_item.link, download_item.host,
                                       download_item.size, download_item.progress, download_item.time, download_item.chunks,
-                                      download_item.video_quality])
+                                      download_item.video_quality, download_item.save_as])
         self.session_parser.save(download_list)
         logger.debug("Session has been saved")
         if len(all_downloads) != len(download_list):
