a85ebd85f558333bebe535ad9a4ce6c255468ece
nitely <ochdownloader@gmail.com>
2013-01-11 22:43:39 -0300

    zippyshare fix

--- plugins/zippyshare/anonym_download.py
+++ plugins/zippyshare/anonym_download.py
@@ -14,8 +14,34 @@ class PluginDownload(PluginsCore):
 
     def parse(self):
         www = self.link.split('.')[0].split('/')[-1]
-        link = 'http://%s.zippyshare.com/' % www
+        link = 'http://%s.zippyshare.com' % www
+
+        self.recaptcha_post_link = link + "/rest/captcha/test"
+        self.recaptcha_challenge_field = "challenge"
+        self.recaptcha_response_field = "response"
+        self.recaptcha_extra_fields = []
+
         page = self.get_page(self.link)
+        m = self.get_match('shortencode: \'(?P<shortencode>[^\']+)', page)
+        shortencode = ("shortencode", m.group('shortencode'))
+        self.recaptcha_extra_fields.append(shortencode)
+        c_pattern = 'Recaptcha\.create\("(?P<key>[^"]+)'
+        page_ = self.recaptcha(c_pattern, page)
+        s_pattern = '(?P<path>[^\']+/%s/[^\']+)' % m.group('shortencode')
+        m = self.get_match(s_pattern, page)
+        dl_link = link + m.group('path')
+        self.source = self.get_page(dl_link, close=False)
+
+    def recaptcha_post(self, pattern, page, challenge, response, extra_fields=None):
+        #overriden
+        form_list = [(self.recaptcha_challenge_field, challenge), (self.recaptcha_response_field, response)]
+        form_list.extend(self.recaptcha_extra_fields)
+        page = self.get_page(self.recaptcha_post_link, form=form_list, default=page)
+        m = self.get_match('false', page) # failure_pattern
+        return m, page
+
+    # Deprecated
+    def parse2(self, link, page):
         time = self.get_time(link, page)
         key = self.link.split('/v/')[-1].split('/')[0] #file id
         data = [('key', key), ('time', time)]
