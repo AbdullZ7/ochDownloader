c1f79c02257771d86d2ea03ab56ab014321030ac
nitely <ochdownloader@gmail.com>
2013-01-12 02:54:03 -0300

    plugin multiupload

---
+++ plugins/multiupload/anonym_download.py
@@ -0,0 +1,32 @@
+#python libs
+import json
+import logging
+logger = logging.getLogger(__name__)
+
+#Libs
+from core.plugins_core import PluginsCore
+
+BASE_URL = "http://multiupload.nl"
+
+
+class PluginDownload(PluginsCore):
+    def __init__(self, *args, **kwargs):
+        PluginsCore.__init__(self, *args, **kwargs)
+
+    def parse(self):
+        file_id = self.link.split("/")[-1]
+
+        self.recaptcha_post_link = self.link + '?c=' + file_id
+
+        page = self.get_page(self.link)
+        c_pattern = 'Recaptcha\.create\("(?P<key>[^"]+)'
+        page = self.recaptcha(c_pattern, page)
+        json_response = json.loads(page)
+        self.source = self.get_page(json_response['href'], close=False)
+
+    def recaptcha_success(self, pattern, page):
+        #overriden
+        if 'href' in page:
+            return True
+        else:
+            return False
