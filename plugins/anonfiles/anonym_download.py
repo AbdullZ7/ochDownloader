43d95a490074226b2ba66ef106e2dada2048aeb7
nitely <ochdownloader@gmail.com>
2013-01-12 02:11:41 -0300

    plugin anonfiles

---
+++ plugins/anonfiles/anonym_download.py
@@ -0,0 +1,20 @@
+#python libs
+import cookielib
+import logging
+logger = logging.getLogger(__name__)
+
+#Libs
+from core.plugins_core import PluginsCore
+
+BASE_URL = "http://anonfiles.com"
+COOKIE = cookielib.CookieJar()
+
+
+class PluginDownload(PluginsCore):
+    def __init__(self, *args, **kwargs):
+        PluginsCore.__init__(self, *args, **kwargs)
+
+    def parse(self):
+        page = self.get_page(self.link)
+        s_pattern = 'href="(?P<link>[^"]+)" class="download_button'
+        self.source = self.click(s_pattern, page, False)
