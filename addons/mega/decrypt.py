846ee5e0e318ff9a28f68625a03a82ea784f86bb
nitely <ochdownloader@gmail.com>
2013-03-17 02:24:21 -0300

    addon mega decrypt

--- addons/mega/decrypt.py
+++ addons/mega/decrypt.py
@@ -1,44 +1,40 @@
+import os
+
 from Crypto.Cipher import AES
 from Crypto.Util import Counter
 
 import crypto
 
+FILE_EXT = ".crypted"
 
-def getfile2(download_item):
+
+def get_out_name(name):
+    if name.endswith(FILE_EXT):
+        return name[:-len(FILE_EXT)]
+    else:
+        return name + ".decrypted"
+
+
+def decrypt(download_item):
     url_parts = download_item.link.split("!")
-    #if len(url_parts) != 3:
-        #raise missing File Key
     file_id, file_key = url_parts[1], url_parts[2]
+
     key = crypto.base64_to_a32(file_key)
     k = (key[0] ^ key[4], key[1] ^ key[5], key[2] ^ key[6], key[3] ^ key[7])
     iv = key[4:6] + (0, 0)
-    meta_mac = key[6:8]
 
-    infile = open('myfile.part', 'rb')
-    outfile = open('myfile', 'wb')
-    decryptor = AES.new(crypto.a32_to_str(k), AES.MODE_CTR, counter=Counter.new(128, initial_value = ((iv[0] << 32) + iv[1]) << 64))
+    file_path = os.path.join(download_item.path, download_item.name)
+    size = os.path.getsize(file_path)
+    out_name = get_out_name(download_item.name)
+    out_file_path = os.path.join(download_item.path, out_name)
 
-    size = None # get_file_size
+    decryptor = AES.new(crypto.a32_to_str(k), AES.MODE_CTR, counter=Counter.new(128, initial_value=((iv[0] << 32) + iv[1]) << 64))
 
-    file_mac = [0, 0, 0, 0]
-    for chunk_start, chunk_size in sorted(crypto.get_chunks(size).items()):
-        chunk = infile.read(chunk_size)
-        chunk = decryptor.decrypt(chunk)
-        outfile.write(chunk)
+    with open(file_path, 'rb') as fh_in:
+        with open(out_file_path, 'wb') as fh_out:
+            for chunk_start, chunk_size in sorted(crypto.get_chunks(size).items()):
+                chunk = fh_in.read(chunk_size)
+                chunk = decryptor.decrypt(chunk)
+                fh_out.write(chunk)
 
-        chunk_mac = [iv[0], iv[1], iv[0], iv[1]]
-        for i in xrange(0, len(chunk), 16):
-            block = chunk[i:i+16]
-            if len(block) % 16:
-                block += '\0' * (16 - (len(block) % 16))
-            block = crypto.str_to_a32(block)
-            chunk_mac = [chunk_mac[0] ^ block[0], chunk_mac[1] ^ block[1], chunk_mac[2] ^ block[2], chunk_mac[3] ^ block[3]]
-            chunk_mac = crypto.aes_cbc_encrypt_a32(chunk_mac, k)
-
-        file_mac = [file_mac[0] ^ chunk_mac[0], file_mac[1] ^ chunk_mac[1], file_mac[2] ^ chunk_mac[2], file_mac[3] ^ chunk_mac[3]]
-        file_mac = crypto.aes_cbc_encrypt_a32(file_mac, k)
-
-    if (file_mac[0] ^ file_mac[1], file_mac[2] ^ file_mac[3]) != meta_mac:
-        print "MAC mismatch"
-    else:
-        print "MAC OK"
+            # mac checking omitted
