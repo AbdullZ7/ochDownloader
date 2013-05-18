467917225005c35628f93ccc529a96ce249a6891
nitely <ochdownloader@gmail.com>
2013-05-18 17:17:32 -0300

    addon mega importer

--- addons/mega/tab.py
+++ addons/mega/tab.py
@@ -1,9 +1,13 @@
 import weakref
+import os
+
+from core.api import api
 
 from PySide.QtGui import *
 from PySide.QtCore import *
 
 from qt.tree_view import TreeView
+from qt import media
 
 
 class Tab(QWidget):
@@ -25,6 +29,21 @@ class Tab(QWidget):
         self.tree_view.setColumnHidden(0, True)
 
         self.vbox.addWidget(self.tree_view)
+
+        self.hbox = QHBoxLayout()
+        self.hbox.setContentsMargins(0, 0, 0, 0)
+
+        self.btn_add = QPushButton()
+        self.btn_add.setIcon(media.get_icon(media.ADD, media.MEDIUM))
+        self.btn_add.setIconSize(QSize(24, 24))
+        self.btn_add.clicked.connect(self.on_import)
+        self.btn_add.setFixedHeight(35)
+        self.btn_add.setMaximumWidth(40)
+
+        self.hbox.addStretch()
+        self.hbox.addWidget(self.btn_add)
+
+        self.vbox.addLayout(self.hbox)
 
     @property
     def parent(self):
@@ -58,7 +77,16 @@ class Tab(QWidget):
             self.parent.tab.setCurrentIndex(tab_index)
 
     def on_import(self):
-        pass
+        dialog = ImportDialog(self.parent)
+        if dialog.result() == QDialog.Accepted:
+            path = dialog.file_line.text()
+            link = dialog.link_line.text()
+            if path and link:
+                file_path, file_name = os.path.split(path)
+                download_item = api.create_download_item(file_name, link)
+                download_item.path = file_path
+                self.decrypt_manager.add(download_item)
+                self.store(download_item)
 
     def store(self, download_item):
         item = [download_item.id, download_item.name, None]
@@ -84,3 +112,59 @@ class Tab(QWidget):
         if not self.decrypt_manager.active_items:
             self.running = False
             self.timer.stop()
+
+
+class ImportDialog(QDialog):
+    def __init__(self, parent):
+        QDialog.__init__(self, parent, Qt.WindowSystemMenuHint | Qt.WindowTitleHint)
+        self.setWindowTitle(_("Mega decrypt importer"))
+        self.resize(340, 200)
+
+        vbox = QVBoxLayout()
+        vbox.setSpacing(20)
+        self.setLayout(vbox)
+
+        hbox = QHBoxLayout()
+
+        self.file_line = QLineEdit()
+        self.file_line.setFixedHeight(35)
+        self.file_line.setMinimumWidth(1)
+
+        hbox.addWidget(self.file_line)
+
+        btn_examine = QPushButton('...')
+        btn_examine.clicked.connect(self.on_examine)
+        btn_examine.setFixedHeight(35)
+        btn_examine.setMaximumWidth(80)
+        hbox.addWidget(btn_examine)
+
+        vbox.addLayout(hbox)
+
+        self.link_line = QLineEdit()
+        vbox.addWidget(self.link_line)
+
+        # buttons
+        vbox.addStretch()
+
+        hbox_btns = QHBoxLayout()
+        vbox.addLayout(hbox_btns)
+
+        hbox_btns.addStretch()
+
+        btn_ok = QPushButton(_('OK'))
+        btn_ok.setDefault(True)
+        btn_ok.clicked.connect(self.accept)
+        btn_ok.setFixedHeight(35)
+        btn_ok.setMaximumWidth(80)
+        hbox_btns.addWidget(btn_ok)
+
+        #if hasattr(widget, 'setFocus'):
+            #widget.setFocus() #call after creating all of the other widgets.
+
+        self.exec_()
+        self.deleteLater()
+
+    def on_examine(self):
+        file_name, filter = QFileDialog.getOpenFileName(filter='Crypted Files (*.crypted)')
+        if file_name:
+            self.file_line.setText(file_name)
