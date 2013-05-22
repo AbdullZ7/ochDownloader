184ba43f63cbe7c970a80b5c161a6980984e4089
nitely <ochdownloader@gmail.com>
2013-05-22 14:44:02 -0300

    plugin ul_to fix

--- plugins/ul_to/anonym_download.py
+++ plugins/ul_to/anonym_download.py
@@ -1,6 +1,5 @@
-from plugins.uploaded.anonym_download import PluginDownload as plugin_download
+from plugins.uploaded_net.anonym_download import PluginDownload as plugin_download
 
 
 class PluginDownload(plugin_download):
-    def __init__(self, *args, **kwargs):
-        plugin_download.__init__(self, *args, **kwargs)
+    """"""
--- plugins/ul_to/link_checker.py
+++ plugins/ul_to/link_checker.py
@@ -1,6 +1,5 @@
-from plugins.uploaded.link_checker import LinkChecker as link_checker
+from plugins.uploaded_net.link_checker import LinkChecker as link_checker
 
 
-class LinkChecker:
-    def check(self, link):
-        return link_checker().check(link)
+class LinkChecker(link_checker):
+    """"""
